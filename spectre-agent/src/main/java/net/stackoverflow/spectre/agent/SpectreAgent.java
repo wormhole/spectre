650a094c725a2272c6f6de422de045585f9da983
wormhole <363408268@qq.com>
2021-01-12 19:49:41 +0800

    SpectreAgent

--- spectre-agent/src/main/java/net/stackoverflow/spectre/agent/SpectreAgent.java
+++ spectre-agent/src/main/java/net/stackoverflow/spectre/agent/SpectreAgent.java
@@ -1,5 +1,9 @@
 package net.stackoverflow.spectre.agent;
 
+import net.stackoverflow.spectre.transport.NettyTransportServer;
+import net.stackoverflow.spectre.transport.TransportClient;
+import net.stackoverflow.spectre.transport.TransportServer;
+
 import java.lang.instrument.Instrumentation;
 
 /**
@@ -10,10 +14,15 @@ import java.lang.instrument.Instrumentation;
 public class SpectreAgent {
 
     public static void premain(String agentArgs, Instrumentation inst) {
-        System.out.println("premain");
+        main(agentArgs, inst);
     }
 
     public static void agentmain(String agentArgs, Instrumentation inst) {
-        System.out.println("agent main");
+        main(agentArgs, inst);
+    }
+
+    private static void main(String agentArgs, Instrumentation inst) {
+        TransportServer server = new NettyTransportServer();
+        server.bind("127.0.0.1", 9966, null);
     }
 }
