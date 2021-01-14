eb08580d109f28110814a4f6bbb443f05b1f718a
wormhole <363408268@qq.com>
2021-01-14 23:51:04 +0800

    bootstrap

--- spectre-shell/src/main/java/net/stackoverflow/spectre/shell/Bootstrap.java
+++ spectre-shell/src/main/java/net/stackoverflow/spectre/shell/Bootstrap.java
@@ -1,10 +1,14 @@
 package net.stackoverflow.spectre.shell;
 
-import com.sun.tools.attach.*;
+import com.sun.tools.attach.AgentInitializationException;
+import com.sun.tools.attach.AgentLoadException;
+import com.sun.tools.attach.AttachNotSupportedException;
+import com.sun.tools.attach.VirtualMachine;
 import net.stackoverflow.spectre.shell.command.*;
 import net.stackoverflow.spectre.shell.util.PrintUtils;
 import net.stackoverflow.spectre.transport.NettyTransportClient;
 import net.stackoverflow.spectre.transport.TransportClient;
+import net.stackoverflow.spectre.transport.command.Invoker;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -21,31 +25,74 @@ public class Bootstrap {
 
     private static final Logger log = LoggerFactory.getLogger(Bootstrap.class);
 
-    public static void main(String[] args) throws IOException, AttachNotSupportedException, AgentLoadException, AgentInitializationException {
-        PrintUtils.printVirtualMachines();
+    private final BufferedReader reader;
+
+    private VirtualMachine vm;
+
+    private TransportClient client;
+
+    public Bootstrap() {
         InputStreamReader isr = new InputStreamReader(System.in);
-        BufferedReader reader = new BufferedReader(isr);
+        this.reader = new BufferedReader(isr);
+    }
+
+    public static void main(String[] args) {
+        Bootstrap bootstrap = new Bootstrap();
+        try {
+            bootstrap.loop(args[0], "127.0.0.1", 9966);
+        } catch (Exception e) {
+            e.printStackTrace();
+            bootstrap.exit(-1);
+        }
+        bootstrap.exit(0);
+    }
+
+    private VirtualMachine attach(String agentJar) throws IOException, AttachNotSupportedException, AgentLoadException, AgentInitializationException {
+        PrintUtils.printVirtualMachines();
         String pid = reader.readLine();
         VirtualMachine vm = VirtualMachine.attach(pid);
-        vm.loadAgent(args[0]);
+        vm.loadAgent(agentJar);
+        return vm;
+    }
 
+    private TransportClient connect(String ip, int port) {
         TransportClient client = new NettyTransportClient();
-        client.connect("127.0.0.1", 9966);
+        client.connect(ip, port);
+        return client;
+    }
 
+    private Invoker initCommand(TransportClient client) {
+        Invoker invoker = new SpectreInvoker();
         SpectreReceiver receiver = new SpectreReceiver(client);
-        SpectreInvoker invoker = new SpectreInvoker();
         invoker.addCommand(new HelpCommand("help", receiver));
         invoker.addCommand(new ThreadCommand("thread", receiver));
         invoker.addCommand(new ExitCommand("exit", receiver));
+        return invoker;
+    }
 
-        PrintUtils.printBanner(pid);
+    public void loop(String agentJar, String ip, int port) throws AgentInitializationException, AgentLoadException, AttachNotSupportedException, IOException {
+        this.vm = attach(agentJar);
+        this.client = connect(ip, port);
+        Invoker invoker = initCommand(this.client);
+
+        PrintUtils.printBanner(vm.id());
         String cmd = null;
         do {
-            PrintUtils.printSession(pid);
+            PrintUtils.printSession(vm.id());
             cmd = reader.readLine();
             invoker.call(cmd.trim());
         } while (!"exit".equals(cmd));
-        client.close();
-        vm.detach();
+    }
+
+    public void exit(int status) {
+        if (this.vm != null) {
+            try {
+                this.vm.detach();
+            } catch (IOException e) {
+                e.printStackTrace();
+                System.exit(-1);
+            }
+        }
+        System.exit(status);
     }
 }
