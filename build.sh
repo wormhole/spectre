75d5d642728f416da6812ca8814e31cf1824fe6c
wormhole <363408268@qq.com>
2021-01-17 00:16:47 +0800

    build

--- build.sh
+++ build.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 
 cd spectre-parent
-call mvn clean install
+mvn clean install
 cd spectre-common
-call mvn clean install
+mvn clean install
 cd ../spectre-transport
-call mvn clean install
+mvn clean install
 cd ../spectre-agent
-call mvn clean install
+mvn clean install
 cd ../spectre-shell
-call mvn clean install
+mvn clean install
 cd ..
 
 rm -rf build
