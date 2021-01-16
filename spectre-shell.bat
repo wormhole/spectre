7dd2cdcfa7c61fca2e005d3dda2bae2630c4fc21
wormhole <363408268@qq.com>
2021-01-16 21:41:11 +0800

    bat

--- spectre-shell.bat
+++ spectre-shell.bat
@@ -4,7 +4,7 @@ if exist "%JAVA_HOME%" (
     if exist "%JAVA_HOME%\lib\tools.jar" (
         "%JAVA_HOME%\bin\java" -Xbootclasspath/a:"%JAVA_HOME%\lib\tools.jar" -jar spectre-shell.jar %~sdp0\spectre-agent.jar
     ) else (
-        echo Could not found tools.jar
+        echo Could not found tools.jar(required)
     )
 ) else (
     echo Could not found variable JAVA_HOME
