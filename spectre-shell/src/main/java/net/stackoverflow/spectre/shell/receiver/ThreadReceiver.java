4792f611b0867327d59f0daee6e7ec5ef30029e8
wormhole <363408268@qq.com>
2021-01-18 16:28:54 +0800

    thread

--- spectre-shell/src/main/java/net/stackoverflow/spectre/shell/receiver/ThreadReceiver.java
+++ spectre-shell/src/main/java/net/stackoverflow/spectre/shell/receiver/ThreadReceiver.java
@@ -47,12 +47,12 @@ public class ThreadReceiver implements Receiver {
 
     private void renderThreads(List<ThreadInfo> infos) {
         System.out.print(Ansi.ansi().fgBlack().bg(Ansi.Color.WHITE).bold());
-        System.out.printf("%-5s  %-24.24s  %-13s  %-13s  %-13s  %-10s  %-8s  %-6s  %-6s  %-11s  %-9s  %-13s  %-12s  %-13s  %-13s",
+        System.out.printf("%-5s  %-24.24s  %-13s  %-13s  %-13s  %-15.15s  %-8s  %-6s  %-6s  %-11s  %-9s  %-13s  %-12s  %-13s  %-13s",
                 "id", "name", "cpu.rate(%)", "cpu.time", "user.time", "group", "priority", "active", "daemon", "interrupted",
                 "suspended", "blocked.count", "waited.count", "lock.owner.id", "state");
         System.out.println(Ansi.ansi().reset());
         for (ThreadInfo info : infos) {
-            AnsiConsole.out().printf("%-5s  %-24.24s  %-13s  %-13s  %-13s  %-10s  %-8s  %-6s  %-6s  %-11s  %-9s  %-13s  %-12s  %-13s  %-13s%n",
+            AnsiConsole.out().printf("%-5s  %-24.24s  %-13s  %-13s  %-13s  %-15.15s  %-8s  %-6s  %-6s  %-11s  %-9s  %-13s  %-12s  %-13s  %-13s%n",
                     info.getThreadId(), info.getThreadName(), String.format("%.2f", info.getCpuRate()), FormatUtils.formatNanoSecond(info.getCpuTime()),
                     FormatUtils.formatNanoSecond(info.getUserTime()), info.getGroup(), info.getPriority(), info.getActive(), info.getDaemon(), info.getInterrupted(), info.getSuspended(),
                     info.getBlockedCount(), info.getWaitedCount(), info.getLockOwnerId(), threadState(info.getThreadState()));
